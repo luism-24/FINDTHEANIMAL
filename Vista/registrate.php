d9b712472b441b8254e4981b5518e0150b28664e
July217 <143135996+July217@users.noreply.github.com>
2023-09-22 19:58:36 -0500

    Modificar formulario de registrarse del usuario acorde a la Base de datos
    Cambié el orden de los campos, los names, agregué la conexión en form y el name del botón registrarse

--- Vista/registrate.php
+++ Vista/registrate.php
@@ -10,87 +10,68 @@ include('Menu.php');
         <p class="section-description">Regístrate para una mejor experiencia.</p>
       </div>
        <div class="row">
-        <form> <center>
-          <div class="form-row">
-            <div class="form-group">
+        <form method="post" action="../Controlador/GuardarRegistrarUsuar.php"> <center>
+        <div class="form-row">
+        <div class="form-group">
               <div class="col-md-6 cl-sm-12 mt-2" >
               <label for="exampleInputEmail1" style="float: left;">Tipo Documento</label>
-              <select name="IdTidocu" id="">
-                <option value=""></option>
-                <option value=""></option>
+              <select id="inputState" class="form-control" name="IdTidocu">
+              <?php  
+                    include( '../Controlador/conex.php'); 
+                    # Consultamos a la tabla tipodocu, que es la que tiene los tipos de docuementos en la BD:
+                    $sql = "SELECT * FROM tipodoc ";
+                    $eje = $conexion->query($sql);
+                    # Mostramos a través de un ciclo todas las opciones válidas:
+                    while($row = $eje->fetch_row()){
+                    echo '<option value="'.$row[0].'">'.$row[1].'</option>';
+                    }
+              ?>
               </select>
               </div>
-            </div> 
+            </div>
             <div class="form-group">
               <div class="col-md-6 cl-sm-12 mt-2" >
               <label for="exampleInputEmail1" style="float: left;">Documento</label>
-              <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Ingrese su nombre documento">
+              <input type="number" class="form-control" name="IdUsuario" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Ingrese su nombre documento">
               </div>
             </div> 
             <div class="form-group">
               <div class="col-md-6 cl-sm-12 mt-2" >
               <label for="exampleInputEmail1" style="float: left;">Nombre</label>
-              <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Ingrese su nombre completo">
+              <input type="text" class="form-control" name="Nombrecomple" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Ingrese su nombre completo">
               </div>
             </div>
             <div class="form-group">
               <div class="col-md-6 cl-sm-12 mt-2" >
               <label for="exampleInputPassword1" style="float: left;">Correo Electrónico</label>
-              <input type="email" class="form-control" id="exampleInputPassword1" placeholder="Ingrese su correo electrónico">
+              <input type="email" class="form-control" name="Correousua" id="exampleInputPassword1" placeholder="Ingrese su correo electrónico">
               </div>
             </div>
           </div>
           <div class="form-group">
             <div class="col-md-6 cl-sm-12 mt-2" >
             <label for="exampleInputPassword1" style="float: left;">Contraseña</label>
-            <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Ingrese una contraseña"> <br>
+            <input type="password" class="form-control" name="Contrausua" id="exampleInputPassword1" placeholder="Ingrese una contraseña"> <br>
             <label for="exampleInputPassword1" style="float: left;">Repita la contraseña</label>
-            <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Repita la contraseña">
-            </div>
-          </div>
-          <div class="form-group">
-            <div class="col-md-6 cl-sm-12 mt-2" >
-            <label for="exampleInputEmail1" style="float: left;">Dirección</label>
-            <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Ingrese su dirección">
+            <input type="password" class="form-control" name="Contrausua" id="exampleInputPassword1" placeholder="Repita la contraseña">
             </div>
           </div>
           <div class="form-group">
             <div class="col-md-6 cl-sm-12 mt-2" >
             <label for="exampleInputEmail1" style="float: left;">Teléfono</label>
-            <input type="number" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Ingrese su teléfono(Ejemplo, +57 302 450 6783)">
+            <input type="number" class="form-control" name="Telefonousua" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Ingrese su teléfono(Ejemplo, +57 302 450 6783)">
             </div>
-          </div>
-          <div class="form-group">
-            <div class="col-md-6 cl-sm-12 mt-2" >
-            <label for="exampleInputEmail1" style="float: left;">Selecciona tu ciudad o municipio</label>
-            <select class="form-control">
-              <option>Selecciona</option>
-              <option>Barbosa</option>
-              <option>Girardota</option>
-              <option>Copacabana</option>
-              <option>Bello</option>
-              <option>Medellín</option>
-              <option>Itagüí</option>
-              <option>Envigado</option>
-              <option>Sabaneta</option>
-              <option>Caldas</option>
-            </select>
-            </div>
-          </div>
-          <div class="form-group">
-            <div class="col-md-6 cl-sm-12 mt-2" >
-            <label for="exampleInputEmail1" style="float: left;">Ingrese su fecha de nacimiento</label>
-            <input type="date" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Ingrese su fecha de nacimiento">
-            </div>
-          </div>
+          </div> 
           <div class="form-group form-check">
             <div class="col-md-6 cl-sm-12">
             <input type="checkbox" class="form-check-input" id="exampleCheck1" style="margin-left: 150px;">
             <label class="form-check-label" for="exampleCheck1" style="margin-right: 140px;">Acepto Términos de Servicio</label>
             </div>
           </div>
+          <button  type="submit" name="BtnGuardar" class="btn btn-primary mt-2" >Regístrate</button>
           <p style="margin-bottom: 5px;">¿Ya tienes cuenta? <a href="Iniciarsesion.php"data-toggle="modal" data-target="#exampleModal2">Iniciar Sesión</a></p>
-          <button type="submit" class="btn btn-primary mt-2">Regístrate</button>
+          <p style="margin-bottom: 5px;">¿No tienes cuenta como empresa? <a href="registrateempre.php"data-toggle="modal" data-target="#exampleModal2">Regístrarte como empresa</a></p>
+
         </form> </center>
       </div>
     </div>
