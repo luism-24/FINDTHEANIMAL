1b734e7a6b953f5742d02787e462182b96a29e81
Nikolle13-Hoyos <143136136+Nikolle13-Hoyos@users.noreply.github.com>
2023-09-27 13:38:48 -0500

    Cambie de menu de index 2

--- Vista/Index2.php
+++ Vista/Index2.php
@@ -1,107 +1,59 @@
 <?php
-session_start();
-error_reporting(0);
+include('Menu.php');
 ?>
-<!DOCTYPE html>
-<html lang="en">
+<section class="container mt-5">
+  <div class="row mt-5">
 
-<head>
-  <meta charset="utf-8">
-  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+  <div class="section-header" style="margin-top: 25px">
+        </div>
+      <!-- Después de crear la fila hacemos la consulta a la tabla que deseamos mostrar datos-->
+      <?php 
+              #Conectamos a la Base de datos
+                    include( '../Controlador/conex.php'); 
+                  #Creamos una variable, $Consulta, que va a contener el Script Select(Consulta a la base de datos)
+                   
+                    $Consulta="SELECT Idanimal, Nombreanimal, Fotoanimal, Descrianimal, Descriraza, Descriestado, Descrimuni FROM animal INNER JOIN raza ON  raza.Idraza = animal.Idraza
+                    INNER JOIN estado ON animal.idestado
+                    INNER JOIN municipio ON municipio.Idmunicipio = animal.Idmunicipio";
+                     #Creamos una variable, $Tour que es envia la conexión y ejecuta la consulta $Consulta
+                    $Tour=$conexion->query($Consulta);
+                     #Creamos un ciclo que permite que los datos de la consulta sean insertados en un vector 
+                     #llamado $row, que se ejecuta mientras se encuentres datos dentro de la tabla que estamos 
+                     #consultando
 
-  <title>Find The Animal </title>
-  <meta content="" name="description">
-  <meta content="" name="keywords">
-  <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
-<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
-<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-<!------ Include the above in your HEAD tag ---------->
+                    while ($row=$Tour->fetch_row()){
+                   ?>
+                     <!-- Insertamos el div que permite determinar el espacio en que estará insertada la columna-->
+           <div class="col-md-12 col-sm-12">
+         <!-- De acuerdo a la versión de Bootstrap que estamos manejando, creamos una sola tarjeta-->
+            <div class="card" style="width: 18rem; margin-top: 25px">
+             <!-- En la etiqueta de imágenes creamos la ruta donde vamos a insertar las imágenes y consultamos 
+             el nombre  de la imagen, en este caso esta en la posición 5; recordemos  que la carpeta que estamos
+            llamando debe contener el nombre y extensión de la imagen que estamos consultando,
+            en este caso, las imágenes deben estar en la carpeta assets/img (Vista)-->                 
+            <img src="App/Admon/img/<?php echo ''.$row[2].''; ?>" class="card-img-top" alt="...">
+            <div class="card-body">
+                 <!-- En la posición 1 está el nombre del sitio -->  
+                <h5 class="card-title" style="color: #3b626f;"><?php echo ''.$row[1].''; ?></h5>
+                 <!-- Consultamos los demás datos desde la base de datos -->  
+                <p class="card-text card-text1">
+                <b>Descripción: </b><?php echo ''.$row[3].''; ?><br>
+                <b>Raza: </b><?php echo ''.$row[4].''; ?><br>
+                <b>Estado: </b><?php echo ''.$row[5].''; ?><br>
+                <b>Municipio: </b><?php echo ''.$row[6].''; ?><br>
+                
 
-<link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN"
-        crossorigin="anonymous">
-        
-<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
-        crossorigin="anonymous"></script>
- <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49"
-        crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy"
-        crossorigin="anonymous"></script>
+                </p>
+            </div>
+            
+            </div>
+            
+         </div>    
+         <?php } ?>
+              <!-- Cerramos las instruccion While donde termina el div de las columnas -->     
+  </div>
 
-  <!-- Favicons -->
-  <link href="assets/img/Find_the_Animal4-removebg-preview.png" rel="icon" class="icono">
-  <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
-
-  <!-- Google Fonts -->
-  <link href="https://fonts.googleapis.com/css?family=Montserrat:300,400,500,700|Open+Sans:300,300i,400,400i,700,700i" rel="stylesheet">
-
-  <!-- Vendor CSS Files -->
-  <link href="assets/vendor/aos/aos.css" rel="stylesheet">
-  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-  <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
-  <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
-
-  <!-- Template Main CSS File -->
-  <link href="assets/css/style.css" rel="stylesheet">
-
-  <!-- =======================================================
-  * Template Name: Avilon
-  * Updated: Mar 09 2023 with Bootstrap v5.2.3
-  * Template URL: https://bootstrapmade.com/avilon-bootstrap-landing-page-template/
-  * Author: BootstrapMade.com
-  * License: https://bootstrapmade.com/license/
-  ======================================================== -->
-</head>
-
-<body>
-
-  <!-- ======= Header ======= -->
-  <header id="header" class="fixed-top d-flex align-items-center header-transparent">
-    <div class="container d-flex justify-content-between align-items-center">
-
-      <div id="logo" style="margin-top: 13px;">
-        <h1><img src="assets/img/Find_the_Animal4-removebg-preview.png" style="width: 50px; height: 50px; margin-top: 20px;"><a href="index.php" style="margin-top: 60%; width: 40px; height: 40px">Find The Animal</a></h1>
-        <p id="p1" style="margin-left: 60px;"></p>
-        <!-- Uncomment below if you prefer to use an image logo -->
-        <!-- <a href="index.html"><img src="assets/img/logo.png" alt=""></a> -->
-      </div>
-
-      <nav id="navbar" class="navbar">
-        <ul>
-          <li><a class="nav-link scrollto active" href="index.php#hero">Inicio</a></li>
-          <li><a class="nav-link scrollto" href="index.php#features">Servicios</a></li>
-    
-        
-       
-          <li class="dropdown"><a href="#"><span>Peluditos</span> <i class="bi bi-chevron-down"></i></a>
-            <ul>
-              
-              <li><a href="Buscados1.php">Perdidos/buscados</a></li>
-                  <li><a href="Vistos.php">Vistos en la calle</a></li>
-                  <li><a href="tiendasasociadas.php">Tiendas asociadas</a></li>
-                  <li><a href="fundaciones.php">Fundaciones</a></li>
-                  <li><a href="veterinarias.php">Veterinarias</a></li>
-              </li>
-             
-            </ul>
-          </li>
-          <li><a class="nav-link scrollto" href="index.php#gallery">Galería</a></li>
-          <li><a class="nav-link scrollto" href="index.php#contact">Contacto</a></li>
-          <li><a class="nav-link scrollto" href="index.php#about">Nosotros</a></li>
-          <li class="dropdown"><a href="#"><span>Perfil</span> <i class="bi bi-chevron-down"></i></a>
-            <ul>
-              <li><a href="../Controlador/logout.php">Cerrar Sesión</a></li>
-                  
-              </li>
-            </ul>
-        </ul>
-        <i class="bi bi-list mobile-nav-toggle"></i>
-      </nav><!-- .navbar -->
-    </div>
-  </header><!-- End Header -->
-
-
-
-    <div class="container-fluid gedf-wrapper">
+    <div class="container-fluid gedf-wrapper" style="margin-top: 100px;">
         <div class="row">
             <div class="col-md-3">
                 <div class="card">
